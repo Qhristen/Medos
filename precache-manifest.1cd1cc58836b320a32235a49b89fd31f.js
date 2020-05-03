6587f5b2d1258f39d5f0046020ef418e7e6e1c93
Qhristen <odeyxtian@gmail.com>
2020-05-03 15:08:12 +0100

    Updates

---
+++ precache-manifest.1cd1cc58836b320a32235a49b89fd31f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f71a561c63ee5898e211560f791ca52",
+    "url": "/Medos/index.html"
+  },
+  {
+    "revision": "b4f2b3199bde291e25f9",
+    "url": "/Medos/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "148a209cb5562e5a3f82",
+    "url": "/Medos/static/css/main.e4e1aceb.chunk.css"
+  },
+  {
+    "revision": "b4f2b3199bde291e25f9",
+    "url": "/Medos/static/js/2.5b697e5f.chunk.js"
+  },
+  {
+    "revision": "11da4d0289ce192632a3fc6590a86fde",
+    "url": "/Medos/static/js/2.5b697e5f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "148a209cb5562e5a3f82",
+    "url": "/Medos/static/js/main.95cf3e80.chunk.js"
+  },
+  {
+    "revision": "85bcd5c6fbb2ec9f4511",
+    "url": "/Medos/static/js/runtime-main.a67a5c63.js"
+  }
+]);
